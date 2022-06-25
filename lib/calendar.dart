a3987edfde735e87d98f6bc68e4299383f37be30
aialeshina@miem.hse.ru <aialeshina@miem.hse.ru>
2022-06-25 12:57:55 +0300

    design change

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -32,7 +32,7 @@ class _CalendarState extends State<Calendar> {
     super.dispose();
   }
 
-  String dropdownValue = 'Meeting';
+  String dropdownValue = 'Встреча';
 
   @override
   Widget build(BuildContext context) {
@@ -43,83 +43,87 @@ class _CalendarState extends State<Calendar> {
       ),
       body: Column(
         children: [
-          TableCalendar(
-            locale: 'ru_RU',
-            startingDayOfWeek: StartingDayOfWeek.monday,
-            focusedDay: selectedDay,
-            firstDay: DateTime(1990),
-            lastDay: DateTime(2050),
-            calendarFormat: format,
-            calendarBuilders: CalendarBuilders(
-              singleMarkerBuilder: (context, date, event) {
-                return Container(
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: (event as Event).status == 'Meeting' ? Colors.blueAccent : (event as Event).status == 'Holiday' ? Colors.lightBlue : Colors.red),
-                  width: 5.0,
-                  height: 5.0,
-                  margin: const EdgeInsets.symmetric(horizontal: 1.5),
-                );
+          Container(
+            child: TableCalendar(
+              locale: 'ru_RU',
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              focusedDay: selectedDay,
+              firstDay: DateTime(1990),
+              lastDay: DateTime(2050),
+              calendarFormat: format,
+              calendarBuilders: CalendarBuilders(
+                singleMarkerBuilder: (context, date, event) {
+                  return Container(
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: (event as Event).status == 'Встреча' ? Colors.blueAccent : (event as Event).status == 'Выходной' ? Colors.lightBlue : Colors.red),
+                    width: 5.0,
+                    height: 5.0,
+                    margin: const EdgeInsets.symmetric(horizontal: 1.5),
+                  );
+                },
+              ),
+              daysOfWeekVisible: true,
+
+              //Day Changed
+              onDaySelected: (DateTime selectDay, DateTime focusDay) {
+                setState(() {
+                  selectedDay = selectDay;
+                  focusedDay = focusDay;
+                });
+                print(focusedDay);
               },
-            ),
-            daysOfWeekVisible: true,
+              selectedDayPredicate: (DateTime date) {
+                return isSameDay(selectedDay, date);
+              },
 
-            //Day Changed
-            onDaySelected: (DateTime selectDay, DateTime focusDay) {
-              setState(() {
-                selectedDay = selectDay;
-                focusedDay = focusDay;
-              });
-              print(focusedDay);
-            },
-            selectedDayPredicate: (DateTime date) {
-              return isSameDay(selectedDay, date);
-            },
+              eventLoader: _getEventsfromDay,
 
-            eventLoader: _getEventsfromDay,
-
-            //To style the Calendar
-            calendarStyle: CalendarStyle(
-              isTodayHighlighted: true,
-              selectedDecoration: BoxDecoration(
-                color: Colors.black12,
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(5.0),
+              //To style the Calendar
+              calendarStyle: CalendarStyle(
+                isTodayHighlighted: true,
+                selectedDecoration: BoxDecoration(
+                  color: Colors.black26,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                selectedTextStyle: TextStyle(color: Colors.white),
+                todayDecoration: BoxDecoration(
+                  color: Colors.black38,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                defaultDecoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                weekendDecoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
               ),
-              selectedTextStyle: TextStyle(color: Colors.white),
-              todayDecoration: BoxDecoration(
-                color: Colors.grey,
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              defaultDecoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              weekendDecoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-            ),
-            headerStyle: HeaderStyle(
-              formatButtonVisible: false,
-              titleCentered: true,
-              formatButtonShowsNext: false,
-              formatButtonDecoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              formatButtonTextStyle: TextStyle(
-                color: Colors.white,
+              headerStyle: HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
+                formatButtonShowsNext: false,
+                formatButtonDecoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                formatButtonTextStyle: TextStyle(
+                  color: Colors.white,
+                ),
               ),
             ),
+            color: Colors.black12,
+            margin: const EdgeInsets.all(10),
           ),
           ..._getEventsfromDay(selectedDay).map(
             (Event event) => ListTile(
               title: Text(
                 event.title,
               ),
-              textColor: (event as Event).status == 'Meeting' ? Colors.blueAccent : (event as Event).status == 'Holiday' ? Colors.lightBlue : Colors.red,
+              textColor: (event as Event).status == 'Встреча' ? Colors.blueAccent : (event as Event).status == 'Выходной' ? Colors.lightBlue : Colors.red,
             ),
           ),
         ],
@@ -131,7 +135,7 @@ class _CalendarState extends State<Calendar> {
           builder: (BuildContext context) {
             int selectedRadio = 0;
             return AlertDialog(
-              title: Text("Add Event"),
+              title: Text("Добавить событие"),
               actions: [
                 TextButton(
                   child: Text("Cancel"),
@@ -176,21 +180,21 @@ class _CalendarState extends State<Calendar> {
                           value: dropdownValue,
                           icon: const Icon(Icons.arrow_downward),
                           elevation: 16,
-                          style: const TextStyle(color: Colors.deepPurple),
+                          style: const TextStyle(color: Colors.black),
                           underline: Container(
                             height: 2,
-                            color: Colors.deepPurpleAccent,
+                            color: Colors.black,
                           ),
                           onChanged: (String newValue) {
                             setState(() {
                               dropdownValue = newValue;
                             });
                           },
-                          items: <String>['Meeting', 'Holiday', 'BirthDay']
+                          items: <String>['Встреча', 'Выходной', 'День Рождения']
                               .map<DropdownMenuItem<String>>((String value) {
                             return DropdownMenuItem<String>(
                               value: value,
-                              child: Text(value),
+                              child: Text(value, style: value == 'Встреча' ? const TextStyle(color: Colors.blueAccent) : value == 'Выходной' ? const TextStyle(color: Colors.lightBlue) : const TextStyle(color: Colors.red)),
                             );
                           }).toList(),
                         )]
@@ -200,7 +204,7 @@ class _CalendarState extends State<Calendar> {
             );
           },
         ),
-        label: Text("Add Event"),
+        label: Text("Добавить событие"),
         icon: Icon(Icons.add),
       ),
 
